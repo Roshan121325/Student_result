5238225a11d66b2ec1cb954ce4ce707c1846d8d7
Roshan121325 <117424315+Roshan121325@users.noreply.github.com>
2024-07-11 22:05:27 +0530

    Add files via upload

---
+++ StudentResults.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+public class StudentResults {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the number of subjects: ");
+        int numberOfSubjects = scanner.nextInt();
+        
+        int[] marks = new int[numberOfSubjects];
+        
+        System.out.println("Enter the marks obtained in each subject (out of 100): ");
+        for (int i = 0; i < numberOfSubjects; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+        
+        // Calculate total marks
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / numberOfSubjects;
+        
+        // Determine grade
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A";
+        } else if (averagePercentage >= 80) {
+            grade = "B";
+        } else if (averagePercentage >= 70) {
+            grade = "C";
+        } else if (averagePercentage >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        
+        // Display results
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+}
